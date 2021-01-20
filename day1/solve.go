0b3f85ba7216b187b77397290d95921775e37502
Michael Coleman <michael.coleman404@gmail.com>
2021-01-20 11:46:39 +1000

    part two

--- day1/solve.go
+++ day1/solve.go
@@ -40,6 +40,19 @@ func solveP1(intList []int) (int, error) {
 	return -1, errors.New("Cannot find solution")
 }
 
+func solveP2(intList []int) (int, error) {
+	for indexA, inputA := range intList {
+		for indexB, inputB := range intList {
+			for indexC, inputC := range intList {
+				if inputA+inputB+inputC == 2020 && indexA != indexB && indexA != indexC && indexC != indexB {
+					return inputA * inputB * inputC, nil
+				}
+			}
+		}
+	}
+	return -1, errors.New("Cannot find solution")
+}
+
 func main() {
 	fmt.Println("Solving Part One!")
 	p1Input, err := readInput("input1.txt")
@@ -51,4 +64,15 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(p1Solution)
+
+	fmt.Println("Solving Part Two!")
+	p2Input, err := readInput("input1.txt")
+	if err != nil {
+		panic(err)
+	}
+	p2Solution, err := solveP2(p2Input)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(p2Solution)
 }
--- day1/solve_test.go
+++ day1/solve_test.go
@@ -6,7 +6,7 @@ import (
 	"testing"
 )
 
-func testSolveP1(t *testing.T) {
+func TestSolveP1(t *testing.T) {
 	var tests = []struct {
 		input []int
 		want  int
@@ -17,6 +17,24 @@ func testSolveP1(t *testing.T) {
 		testname := fmt.Sprintf("%s", strings.Trim(strings.Join(strings.Fields(fmt.Sprint(test.input)), ","), "[]"))
 		t.Run(testname, func(t *testing.T) {
 			ans, _ := solveP1(test.input)
+			if ans != test.want {
+				t.Errorf("got %d, want %d", ans, test.want)
+			}
+		})
+	}
+}
+
+func TestSolveP2(t *testing.T) {
+	var tests = []struct {
+		input []int
+		want  int
+	}{
+		{[]int{1721, 979, 366, 299, 675, 1456}, 241861950},
+	}
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", strings.Trim(strings.Join(strings.Fields(fmt.Sprint(test.input)), ","), "[]"))
+		t.Run(testname, func(t *testing.T) {
+			ans, _ := solveP2(test.input)
 			if ans != test.want {
 				t.Errorf("got %d, want %d", ans, test.want)
 			}
