c860fffbb7143a6b230ca06ab1480fe57d111262
Michael Coleman <michael.coleman404@gmail.com>
2021-01-22 15:05:56 +1000

    day7 part2

--- day7/solve.go
+++ day7/solve.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
@@ -17,7 +18,6 @@ func readInput(filename string) (returnString string) {
 
 func sumParents(bagMap map[string]map[string]bool, bagName string) map[string]bool {
 	uniqueBags := make(map[string]bool)
-	fmt.Println(bagName, bagMap[bagName])
 	for childBag := range bagMap[bagName] {
 		uniqueBags[childBag] = true
 		if bagMap[childBag] == nil {
@@ -30,6 +30,8 @@ func sumParents(bagMap map[string]map[string]bool, bagName string) map[string]bo
 	return uniqueBags
 }
 
+// In part one we build a map where the child bag is the key and the parent bags are the children.
+// We do this so we can easily see which bag is contained by other bags, rather than searching the entire map
 func solveP1(inputString string) int {
 	bagMap := make(map[string]map[string]bool)
 	rules := strings.Split(inputString, "\n")
@@ -52,25 +54,48 @@ func solveP1(inputString string) int {
 	return len(sumParents(bagMap, "shiny gold"))
 }
 
-// func solveP2(inputString string) int {
-// 	answerCount := 0
-// 	groups := strings.Split(inputString, "\n\n")
-// 	for _, group := range groups {
-// 		answerMap := make(map[rune]int)
-// 		members := strings.Split(group, "\n")
-// 		for _, member := range members {
-// 			for _, answer := range member {
-// 				answerMap[answer] = answerMap[answer] + 1
-// 			}
-// 		}
-// 		for answer := range answerMap {
-// 			if answerMap[answer] == len(members) {
-// 				answerCount = answerCount + 1
-// 			}
-// 		}
-// 	}
-// 	return answerCount
-// }
+type bagCount struct {
+	count int
+	name  string
+}
+
+func countChildren(bagMap map[string][]bagCount, bagName string) int {
+	count := 0
+	for _, childbag := range bagMap[bagName] {
+		if bagMap[childbag.name] == nil {
+			count = count + childbag.count
+		} else {
+			count = count + (childbag.count * countChildren(bagMap, childbag.name))
+		}
+	}
+	return count + 1
+}
+
+// in part two the map is in the more sensible order (parent bag is the key) - however here we must use a struct for the children to contain the count.
+func solveP2(inputString string) int {
+	bagMap := make(map[string][]bagCount)
+	rules := strings.Split(inputString, "\n")
+	for _, rule := range rules {
+		sep := strings.Split(rule, " contain ")
+		parentBag := strings.ReplaceAll(sep[0], " bags", "")
+		children := strings.Split(sep[1], ", ")
+		for _, child := range children {
+			if child == "no other bags." {
+				continue
+			}
+			r, _ := regexp.Compile(`^(\d)\s(\w*\s\w*)\sbags?.?$`)
+			childCount, _ := strconv.Atoi(r.FindStringSubmatch(child)[1])
+			childName := r.FindStringSubmatch(child)[2]
+
+			if bagMap[parentBag] == nil {
+				bagMap[parentBag] = make([]bagCount, 0)
+			}
+			bagMap[parentBag] = append(bagMap[parentBag], bagCount{count: childCount, name: childName})
+		}
+	}
+
+	return countChildren(bagMap, "shiny gold") - 1 //minus one for self
+}
 
 func main() {
 	fmt.Println("Solving Part One!")
@@ -78,8 +103,8 @@ func main() {
 	p1Solution := solveP1(p1Input)
 	fmt.Println(p1Solution)
 
-	// fmt.Println("Solving Part Two!")
-	// p2Input := readInput("input.txt")
-	// p2Solution := solveP2(p2Input)
-	// fmt.Println(p2Solution)
+	fmt.Println("Solving Part Two!")
+	p2Input := readInput("input.txt")
+	p2Solution := solveP2(p2Input)
+	fmt.Println(p2Solution)
 }
--- day7/solve_test.go
+++ day7/solve_test.go
@@ -32,35 +32,27 @@ dotted black bags contain no other bags.`, 4},
 	}
 }
 
-// func TestSolveP2(t *testing.T) {
-// 	var tests = []struct {
-// 		input string
-// 		want  int
-// 	}{
-// 		{
-// 			`abc
-
-// a
-// b
-// c
-
-// ab
-// ac
-
-// a
-// a
-// a
-// a
-
-// b`, 6},
-// 	}
-// 	for _, test := range tests {
-// 		testname := fmt.Sprintf("%s", test.input)
-// 		t.Run(testname, func(t *testing.T) {
-// 			ans := solveP2(test.input)
-// 			if ans != test.want {
-// 				t.Errorf("got %d, want %d", ans, test.want)
-// 			}
-// 		})
-// 	}
-// }
+func TestSolveP2(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{
+			`shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`, 126},
+	}
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", test.input)
+		t.Run(testname, func(t *testing.T) {
+			ans := solveP2(test.input)
+			if ans != test.want {
+				t.Errorf("got %d, want %d", ans, test.want)
+			}
+		})
+	}
+}
