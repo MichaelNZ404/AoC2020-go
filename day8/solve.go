f4a5b9812f0ee6c27ea19c9931a06d73e4c4e799
Michael Coleman <michael.coleman404@gmail.com>
2021-01-23 18:28:26 +1000

    day8 part2

--- day8/solve.go
+++ day8/solve.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"regexp"
@@ -58,14 +59,69 @@ func solveP1(inputString string) int {
 	return acc
 }
 
+func solveP2(inputString string) int {
+	r, _ := regexp.Compile(`^(\w{3})\s(.\d+)$`)
+	rawInstructions := strings.Split(inputString, "\n")
+
+	// build instruction array
+	instructions := make([]instruction, 0)
+	for _, rawInstruction := range rawInstructions {
+		phrase := r.FindStringSubmatch(rawInstruction)[1]
+		quantity, _ := strconv.Atoi(r.FindStringSubmatch(rawInstruction)[2])
+		instructions = append(instructions, instruction{phrase: phrase, quantity: quantity})
+	}
+
+	replaceInstructionNumber := -1
+	for replaceInstructionNumber < len(instructions) {
+		replaceInstructionNumber = replaceInstructionNumber + 1
+		if instructions[replaceInstructionNumber].phrase == "acc" {
+			continue //only runs against nop or jmp can be valid
+		}
+
+		acc = 0
+		linesSeen := make(map[int]bool)
+		currentLineNum := 0
+		for linesSeen[currentLineNum] != true {
+			if currentLineNum >= len(instructions) {
+				return acc
+			}
+			linesSeen[currentLineNum] = true
+
+			phrase := instructions[currentLineNum].phrase
+			if currentLineNum == replaceInstructionNumber {
+				if phrase == "nop" {
+					phrase = "jmp"
+				} else if phrase == "jmp" {
+					phrase = "nop"
+				}
+			}
+
+			if phrase == "nop" {
+				currentLineNum = currentLineNum + 1
+				continue
+			}
+			if phrase == "acc" {
+				acc = acc + instructions[currentLineNum].quantity
+				currentLineNum = currentLineNum + 1
+				continue
+			}
+			if phrase == "jmp" {
+				currentLineNum = currentLineNum + instructions[currentLineNum].quantity
+				continue
+			}
+		}
+	}
+	panic(errors.New("Instruction scan never returned valid result"))
+}
+
 func main() {
 	fmt.Println("Solving Part One!")
 	p1Input := readInput("input.txt")
 	p1Solution := solveP1(p1Input)
 	fmt.Println(p1Solution)
 
-	// fmt.Println("Solving Part Two!")
-	// p2Input := readInput("input.txt")
-	// p2Solution := solveP2(p2Input)
-	// fmt.Println(p2Solution)
+	fmt.Println("Solving Part Two!")
+	p2Input := readInput("input.txt")
+	p2Solution := solveP2(p2Input)
+	fmt.Println(p2Solution)
 }
--- day8/solve_test.go
+++ day8/solve_test.go
@@ -32,27 +32,29 @@ acc +6`, 5},
 	}
 }
 
-// func TestSolveP2(t *testing.T) {
-// 	var tests = []struct {
-// 		input string
-// 		want  int
-// 	}{
-// 		{
-// 			`shiny gold bags contain 2 dark red bags.
-// dark red bags contain 2 dark orange bags.
-// dark orange bags contain 2 dark yellow bags.
-// dark yellow bags contain 2 dark green bags.
-// dark green bags contain 2 dark blue bags.
-// dark blue bags contain 2 dark violet bags.
-// dark violet bags contain no other bags.`, 126},
-// 	}
-// 	for _, test := range tests {
-// 		testname := fmt.Sprintf("%s", test.input)
-// 		t.Run(testname, func(t *testing.T) {
-// 			ans := solveP2(test.input)
-// 			if ans != test.want {
-// 				t.Errorf("got %d, want %d", ans, test.want)
-// 			}
-// 		})
-// 	}
-// }
+func TestSolveP2(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{
+			`nop +0
+acc +1
+jmp +4
+acc +3
+jmp -3
+acc -99
+acc +1
+jmp -4
+acc +6`, 8},
+	}
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", test.input)
+		t.Run(testname, func(t *testing.T) {
+			ans := solveP2(test.input)
+			if ans != test.want {
+				t.Errorf("got %d, want %d", ans, test.want)
+			}
+		})
+	}
+}
