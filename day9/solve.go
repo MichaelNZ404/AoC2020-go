30202d1cb1f5e11b1b7cab99bbf50eea837aef86
Michael Coleman <michael.coleman404@gmail.com>
2021-01-23 20:16:49 +1000

    day9 part2

--- day9/solve.go
+++ day9/solve.go
@@ -50,14 +50,86 @@ func solveP1(inputString string, preambleLength int) int {
 	panic(errors.New("We made it through"))
 }
 
+func solveP2(inputString string, preambleLength int) int {
+	stringList := strings.Split(inputString, "\n")
+	intList := make([]int, len(stringList))
+	for sIndex, sInput := range stringList {
+		newInt, err := strconv.Atoi(sInput)
+		if err != nil {
+			panic(err)
+		}
+		intList[sIndex] = newInt
+	}
+
+	// get the p1 solution
+	targetValue := -1
+	currentLoc := preambleLength
+	for currentLoc < len(intList) {
+		if targetValue > 0 {
+			break
+		}
+		intsToCheck := intList[currentLoc-preambleLength : currentLoc]
+		solution := false
+		for i, iValue := range intsToCheck {
+			if solution {
+				break
+			}
+			for j, jValue := range intsToCheck {
+				if i != j && iValue+jValue == intList[currentLoc] {
+					solution = true
+					break
+				}
+			}
+		}
+		if !solution {
+			targetValue = intList[currentLoc]
+		}
+		currentLoc = currentLoc + 1
+	}
+
+	// find the solution range
+	rangeLower := -1
+	rangeUpper := -1
+	for i, iValue := range intList {
+		if rangeLower > 0 || rangeUpper > 0 {
+			break
+		}
+		thisISum := iValue
+		for j, jValue := range intList[i+1:] {
+			thisISum = thisISum + jValue
+			if thisISum == targetValue {
+				rangeLower = i
+				rangeUpper = j + i + 2
+				break
+			}
+			if thisISum > targetValue {
+				break
+			}
+		}
+	}
+
+	// find the min & max of this range
+	rangeMin := intList[rangeLower]
+	rangeMax := intList[rangeLower]
+	for _, val := range intList[rangeLower:rangeUpper] {
+		if val > rangeMax {
+			rangeMax = val
+		}
+		if val < rangeMin {
+			rangeMin = val
+		}
+	}
+	return rangeMin + rangeMax
+}
+
 func main() {
 	fmt.Println("Solving Part One!")
 	p1Input := readInput("input.txt")
 	p1Solution := solveP1(p1Input, 25)
 	fmt.Println(p1Solution)
 
-	// fmt.Println("Solving Part Two!")
-	// p2Input := readInput("input.txt")
-	// p2Solution := solveP2(p2Input)
-	// fmt.Println(p2Solution)
+	fmt.Println("Solving Part Two!")
+	p2Input := readInput("input.txt")
+	p2Solution := solveP2(p2Input, 25)
+	fmt.Println(p2Solution)
 }
--- day9/solve_test.go
+++ day9/solve_test.go
@@ -42,3 +42,41 @@ func TestSolveP1(t *testing.T) {
 		})
 	}
 }
+
+func TestSolveP2(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{
+			`35
+20
+15
+25
+47
+40
+62
+55
+65
+95
+102
+117
+150
+182
+127
+219
+299
+277
+309
+576`, 62},
+	}
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", test.input)
+		t.Run(testname, func(t *testing.T) {
+			ans := solveP2(test.input, 5)
+			if ans != test.want {
+				t.Errorf("got %d, want %d", ans, test.want)
+			}
+		})
+	}
+}
