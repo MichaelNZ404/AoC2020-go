ba01b4e9c4a8cce47dbddff6a3b79bbd64d366a7
Michael Coleman <michael.coleman404@gmail.com>
2021-01-25 12:13:27 +1000

    day10 part2

--- day10/solve.go
+++ day10/solve.go
@@ -49,14 +49,104 @@ func solveP1(inputString string) int {
 	return oneDiff * threeDiff
 }
 
+// func getSubtreeCount(currentVal int, targetVal int, intList []int) int {
+// 	count := 0
+// 	for idx, iVal := range intList {
+// 		if iVal-currentVal > 3 {
+// 			break
+// 		} else {
+// 			count = count + getSubtreeCount(iVal, targetVal, intList[idx+1:])
+// 		}
+// 		if targetVal-iVal < 4 {
+// 			count = count + 1
+// 		}
+// 	}
+// 	return count
+// }
+
+// This solution has bad performance
+// func solveP2(inputString string) int {
+// 	stringList := strings.Split(inputString, "\n")
+// 	intList := make([]int, len(stringList))
+// 	for sIndex, sInput := range stringList {
+// 		newInt, err := strconv.Atoi(sInput)
+// 		if err != nil {
+// 			panic(err)
+// 		}
+// 		intList[sIndex] = newInt
+// 	}
+// 	sort.Ints(intList)
+
+// 	currentVal := 0
+// 	targetVal := intList[len(intList)-1] + 3
+
+// 	return getSubtreeCount(currentVal, targetVal, intList)
+// }
+
+func getSubtreeCount(currentVal int, targetVal int, intList []int) int {
+	if len(intList) == 1 {
+		return 1
+	}
+
+	count := 0
+	for idx := 1; idx < len(intList); idx++ {
+		iVal := intList[idx]
+		if iVal-currentVal > 3 {
+			break
+		}
+		if iVal == targetVal {
+			count = count + 1
+		} else {
+			count = count + getSubtreeCount(iVal, targetVal, intList[idx:])
+		}
+	}
+	return count
+}
+
+func solveP2(inputString string) int {
+	stringList := strings.Split(inputString, "\n")
+	intList := make([]int, len(stringList))
+	for sIndex, sInput := range stringList {
+		newInt, err := strconv.Atoi(sInput)
+		if err != nil {
+			panic(err)
+		}
+		intList[sIndex] = newInt
+	}
+	intList = append(intList, 0)
+	sort.Ints(intList)
+	intList = append(intList, intList[len(intList)-1]+3)
+	count := 1
+
+	lastSplit := 0
+	subproblems := make([][]int, 0)
+	for idx, iVal := range intList {
+		if idx+1 > len(intList)-1 {
+			subproblems = append(subproblems, intList[lastSplit:])
+			break
+		}
+		if iVal+3 == intList[idx+1] {
+			subproblems = append(subproblems, intList[lastSplit:idx+1])
+			lastSplit = idx + 1
+		}
+	}
+
+	for _, subproblem := range subproblems {
+		subResult := getSubtreeCount(subproblem[0], subproblem[len(subproblem)-1], subproblem)
+		// fmt.Println(subproblem, subResult)
+		count = count * subResult
+	}
+	return count
+}
+
 func main() {
 	fmt.Println("Solving Part One!")
 	p1Input := readInput("input.txt")
 	p1Solution := solveP1(p1Input)
 	fmt.Println(p1Solution)
 
-	// fmt.Println("Solving Part Two!")
-	// p2Input := readInput("input.txt")
-	// p2Solution := solveP2(p2Input)
-	// fmt.Println(p2Solution)
+	fmt.Println("Solving Part Two!")
+	p2Input := readInput("input.txt")
+	p2Solution := solveP2(p2Input)
+	fmt.Println(p2Solution)
 }
--- day10/solve_test.go
+++ day10/solve_test.go
@@ -82,3 +82,81 @@ func TestSolveP1Long(t *testing.T) {
 		})
 	}
 }
+
+func TestSolveP2(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{
+			`16
+10
+15
+5
+1
+11
+7
+19
+6
+12
+4`, 8},
+	}
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", test.input)
+		t.Run(testname, func(t *testing.T) {
+			ans := solveP2(test.input)
+			if ans != test.want {
+				t.Errorf("got %d, want %d", ans, test.want)
+			}
+		})
+	}
+}
+
+func TestSolveP2Long(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{
+			`28
+33
+18
+42
+31
+14
+46
+20
+48
+47
+24
+23
+49
+45
+19
+38
+39
+11
+1
+32
+25
+35
+8
+17
+7
+9
+4
+2
+34
+10
+3`, 19208},
+	}
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", test.input)
+		t.Run(testname, func(t *testing.T) {
+			ans := solveP2(test.input)
+			if ans != test.want {
+				t.Errorf("got %d, want %d", ans, test.want)
+			}
+		})
+	}
+}
