a9670943a7942e26319bac1de0cf42534cc41ade
Michael Coleman <michael.coleman404@gmail.com>
2021-01-25 20:31:37 +1000

    day12 part1

---
+++ day12/solve.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+func readInput(filename string) (returnString string) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	return string(data)
+}
+
+type instructionStruct struct {
+	command string
+	value   int
+}
+
+func solveP1(inputString string) int {
+	lines := strings.Split(inputString, "\n")
+	r, _ := regexp.Compile(`^(\w)(\d+)$`)
+	instructions := make([]instructionStruct, len(lines))
+	for idx, line := range lines {
+		command := r.FindStringSubmatch(line)[1]
+		value, _ := strconv.Atoi(r.FindStringSubmatch(line)[2])
+		instructions[idx] = instructionStruct{command: command, value: value}
+	}
+
+	shipDir := 90 //assuming roations are multiples of 90 degrees
+	shipX, shipY := 0, 0
+
+	for _, instruction := range instructions {
+		switch instruction.command {
+		case "F":
+			switch shipDir {
+			case 0:
+				shipY += instruction.value
+				break
+			case 90:
+				shipX += instruction.value
+				break
+			case 180:
+				shipY -= instruction.value
+				break
+			case 270:
+				shipX -= instruction.value
+				break
+			}
+			break
+		case "N":
+			shipY += instruction.value
+			break
+		case "S":
+			shipY -= instruction.value
+			break
+		case "E":
+			shipX += instruction.value
+			break
+		case "W":
+			shipX -= instruction.value
+			break
+		case "L":
+			shipDir = (shipDir - instruction.value) % 360
+			if shipDir < 0 {
+				shipDir += 360
+			}
+			break
+		case "R":
+			shipDir = (shipDir + instruction.value) % 360
+			if shipDir < 0 {
+				shipDir += 360
+			}
+			break
+		}
+	}
+	return int(math.Abs(float64(shipX)) + math.Abs(float64(shipY)))
+}
+
+func main() {
+	fmt.Println("Solving Part One!")
+	p1Input := readInput("input.txt")
+	p1Solution := solveP1(p1Input)
+	fmt.Println(p1Solution)
+
+	// fmt.Println("Solving Part Two!")
+	// p2Input := readInput("input.txt")
+	// p2Solution := solveP2(p2Input)
+	// fmt.Println(p2Solution)
+}
---
+++ day12/solve_test.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestSolveP1(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{
+			`F10
+N3
+F7
+R90
+F11`, 25},
+	}
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", test.input)
+		t.Run(testname, func(t *testing.T) {
+			ans := solveP1(test.input)
+			if ans != test.want {
+				t.Errorf("got %d, want %d", ans, test.want)
+			}
+		})
+	}
+}
