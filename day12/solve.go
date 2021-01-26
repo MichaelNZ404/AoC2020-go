096bf31be71a2e2996ffa86e47f6224875d79e54
Michael Coleman <michael.coleman404@gmail.com>
2021-01-26 10:18:58 +1000

    day12 part2

--- day12/solve.go
+++ day12/solve.go
@@ -82,14 +82,76 @@ func solveP1(inputString string) int {
 	return int(math.Abs(float64(shipX)) + math.Abs(float64(shipY)))
 }
 
+func rotateRight(x int, y int, quantity int) (returnX int, returnY int) {
+	for i := quantity / 90; i > 0; i-- {
+		oldX := x
+		oldY := y
+		x = oldY
+		y = -oldX
+	}
+	return x, y
+}
+
+func rotateLeft(x int, y int, quantity int) (returnX int, returnY int) {
+	for i := quantity / 90; i > 0; i-- {
+		oldX := x
+		oldY := y
+		x = -oldY
+		y = oldX
+	}
+	return x, y
+}
+
+func solveP2(inputString string) int {
+	lines := strings.Split(inputString, "\n")
+	r, _ := regexp.Compile(`^(\w)(\d+)$`)
+	instructions := make([]instructionStruct, len(lines))
+	for idx, line := range lines {
+		command := r.FindStringSubmatch(line)[1]
+		value, _ := strconv.Atoi(r.FindStringSubmatch(line)[2])
+		instructions[idx] = instructionStruct{command: command, value: value}
+	}
+
+	shipX, shipY := 0, 0
+	waypointOffsetX, waypointOffsetY := 10, 1
+
+	for _, instruction := range instructions {
+		switch instruction.command {
+		case "F":
+			shipX += (waypointOffsetX * instruction.value)
+			shipY += (waypointOffsetY * instruction.value)
+			break
+		case "N":
+			waypointOffsetY += instruction.value
+			break
+		case "S":
+			waypointOffsetY -= instruction.value
+			break
+		case "E":
+			waypointOffsetX += instruction.value
+			break
+		case "W":
+			waypointOffsetX -= instruction.value
+			break
+		case "L":
+			waypointOffsetX, waypointOffsetY = rotateLeft(waypointOffsetX, waypointOffsetY, instruction.value)
+			break
+		case "R":
+			waypointOffsetX, waypointOffsetY = rotateRight(waypointOffsetX, waypointOffsetY, instruction.value)
+			break
+		}
+	}
+	return int(math.Abs(float64(shipX)) + math.Abs(float64(shipY)))
+}
+
 func main() {
 	fmt.Println("Solving Part One!")
 	p1Input := readInput("input.txt")
 	p1Solution := solveP1(p1Input)
 	fmt.Println(p1Solution)
 
-	// fmt.Println("Solving Part Two!")
-	// p2Input := readInput("input.txt")
-	// p2Solution := solveP2(p2Input)
-	// fmt.Println(p2Solution)
+	fmt.Println("Solving Part Two!")
+	p2Input := readInput("input.txt")
+	p2Solution := solveP2(p2Input)
+	fmt.Println(p2Solution)
 }
--- day12/solve_test.go
+++ day12/solve_test.go
@@ -27,3 +27,26 @@ F11`, 25},
 		})
 	}
 }
+
+func TestSolveP2(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{
+			`F10
+N3
+F7
+R90
+F11`, 286},
+	}
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", test.input)
+		t.Run(testname, func(t *testing.T) {
+			ans := solveP2(test.input)
+			if ans != test.want {
+				t.Errorf("got %d, want %d", ans, test.want)
+			}
+		})
+	}
+}
