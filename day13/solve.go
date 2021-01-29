e36578a7b8d10d54ce85e2943cfe9f2367c868ba
Michael Coleman <michael.coleman404@gmail.com>
2021-01-29 13:45:12 +1000

    day13 part2

--- day13/solve.go
+++ day13/solve.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math"
+	"math/big"
 	"strconv"
 	"strings"
 )
@@ -26,7 +27,6 @@ func solveP1(inputString string) int {
 		if val != "x" {
 			busID, _ := strconv.Atoi(val)
 			candidateTime := int(math.Ceil(float64(earliestTime)/float64(busID))) * busID
-			fmt.Println(candidateTime)
 			if minTimeBusID == 0 || candidateTime < minTime {
 				minTime = candidateTime
 				minTimeBusID = busID
@@ -36,14 +36,86 @@ func solveP1(inputString string) int {
 	return minTimeBusID * (minTime - earliestTime)
 }
 
+// slow solution
+// func solveP2(inputString string) int {
+// 	lines := strings.Split(inputString, "\n")
+
+// 	busIds := make([]int, 0)
+// 	for _, val := range strings.Split(lines[1], ",") {
+// 		if val != "x" {
+// 			busID, _ := strconv.Atoi(val)
+// 			busIds = append(busIds, busID)
+// 		} else {
+// 			busIds = append(busIds, 0)
+// 		}
+// 	}
+
+// 	candidateTime := 0
+// 	lastCheckedMult := 0 //this is the last multiple of index 0 checked, keeps our place in the loop
+// 	i := 0
+// 	for i < len(busIds) {
+// 		if i == 0 {
+// 			lastCheckedMult++
+// 			candidateTime = lastCheckedMult * busIds[0]
+// 		} else {
+// 			if busIds[i] == 0 { // this is an 'x'
+// 				i++
+// 				continue
+// 			}
+// 			if (candidateTime+i)%busIds[i] != 0 { // invalid
+// 				i = 0
+// 				continue
+// 			}
+// 		}
+// 		i++
+// 	}
+// 	return candidateTime
+// }
+
+//https://rosettacode.org/wiki/Chinese_remainder_theorem#Go
+var one = big.NewInt(1)
+
+func crt(a, n []*big.Int) (*big.Int, error) {
+	p := new(big.Int).Set(n[0])
+	for _, n1 := range n[1:] {
+		p.Mul(p, n1)
+	}
+	var x, q, s, z big.Int
+	for i, n1 := range n {
+		q.Div(p, n1)
+		z.GCD(nil, &s, n1, &q)
+		if z.Cmp(one) != 0 {
+			return nil, fmt.Errorf("%d not coprime", n1)
+		}
+		x.Add(&x, s.Mul(a[i], s.Mul(&s, &q)))
+	}
+	return x.Mod(&x, p), nil
+}
+
+func solveP2(inputString string) int {
+	lines := strings.Split(inputString, "\n")
+
+	busIds := make([]*big.Int, 0)
+	busRemainders := make([]*big.Int, 0)
+	for idx, val := range strings.Split(lines[1], ",") {
+		if val != "x" {
+			busID, _ := strconv.Atoi(val)
+			busIds = append(busIds, big.NewInt(int64(busID)))
+			busRemainders = append(busRemainders, big.NewInt(int64(-idx)))
+		}
+	}
+	result, _ := crt(busRemainders, busIds)
+	return int(result.Int64())
+}
+
 func main() {
 	fmt.Println("Solving Part One!")
 	p1Input := readInput("input.txt")
 	p1Solution := solveP1(p1Input)
 	fmt.Println(p1Solution)
 
-	// fmt.Println("Solving Part Two!")
-	// p2Input := readInput("input.txt")
-	// p2Solution := solveP2(p2Input)
-	// fmt.Println(p2Solution)
+	fmt.Println("Solving Part Two!")
+	p2Input := readInput("input.txt")
+	p2Solution := solveP2(p2Input)
+	fmt.Println(p2Solution)
 }
--- day13/solve_test.go
+++ day13/solve_test.go
@@ -24,3 +24,38 @@ func TestSolveP1(t *testing.T) {
 		})
 	}
 }
+
+func TestSolveP2(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{
+			`939
+7,13,x,x,59,x,31,19`, 1068781},
+		{
+			`939
+17,x,13,19`, 3417},
+		{
+			`939
+67,7,59,61`, 754018},
+		{
+			`939
+67,x,7,59,61`, 779210},
+		{
+			`939
+67,7,x,59,61`, 1261476},
+		{
+			`939
+1789,37,47,1889`, 1202161486},
+	}
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", test.input)
+		t.Run(testname, func(t *testing.T) {
+			ans := solveP2(test.input)
+			if ans != test.want {
+				t.Errorf("got %d, want %d", ans, test.want)
+			}
+		})
+	}
+}
