aa27add903507d1841b26f42cc7c1116d91fb746
Michael Coleman <michael.coleman404@gmail.com>
2021-01-30 00:27:25 +1000

    day15 part2

--- day15/solve.go
+++ day15/solve.go
@@ -47,14 +47,46 @@ func solveP1(inputString string) int {
 	return thisNumberSpoken
 }
 
+func solveP2(inputString string) int {
+	values := strings.Split(inputString, ",")
+	numberMap := make(map[int]int)
+	turnCount := 0 //AKA spoken count
+	nextNumberSpoken := 0
+
+	for idx, value := range values {
+		number, _ := strconv.Atoi(value)
+		if _, exists := numberMap[number]; !exists {
+			nextNumberSpoken = 0
+		} else {
+			nextNumberSpoken = turnCount - numberMap[number]
+		}
+		numberMap[number] = idx + 1
+	}
+
+	turnCount = len(values)
+	thisNumberSpoken := 0
+	for turnCount < 30000000 {
+		turnCount++
+		thisNumberSpoken = nextNumberSpoken
+
+		if _, exists := numberMap[nextNumberSpoken]; !exists {
+			nextNumberSpoken = 0
+		} else {
+			nextNumberSpoken = turnCount - numberMap[nextNumberSpoken]
+		}
+		numberMap[thisNumberSpoken] = turnCount
+	}
+	return thisNumberSpoken
+}
+
 func main() {
 	fmt.Println("Solving Part One!")
 	p1Input := readInput("input.txt")
 	p1Solution := solveP1(p1Input)
 	fmt.Println(p1Solution)
 
-	// fmt.Println("Solving Part Two!")
-	// p2Input := readInput("input.txt")
-	// p2Solution := solveP2(p2Input)
-	// fmt.Println(p2Solution)
+	fmt.Println("Solving Part Two!")
+	p2Input := readInput("input.txt")
+	p2Solution := solveP2(p2Input)
+	fmt.Println(p2Solution)
 }
--- day15/solve_test.go
+++ day15/solve_test.go
@@ -28,3 +28,27 @@ func TestSolveP1(t *testing.T) {
 		})
 	}
 }
+
+func TestSolveP2(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{`0,3,6`, 175594},
+		{`1,3,2`, 2578},
+		{`2,1,3`, 3544142},
+		{`1,2,3`, 261214},
+		{`2,3,1`, 6895259},
+		{`3,2,1`, 18},
+		{`3,1,2`, 362},
+	}
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", test.input)
+		t.Run(testname, func(t *testing.T) {
+			ans := solveP2(test.input)
+			if ans != test.want {
+				t.Errorf("got %d, want %d", ans, test.want)
+			}
+		})
+	}
+}
