a5d83b9de17ce7b19885ddac1da7071be993693b
Michael Coleman <michael.coleman404@gmail.com>
2021-01-30 23:06:54 +1000

    day18 part2

--- day18/solve.go
+++ day18/solve.go
@@ -61,14 +61,76 @@ func solveMath(equation string) int {
 	return result
 }
 
+func solveAdvancedMath(equation string) int {
+	// solve bracket subproblems recursively
+	firstBracketIdx := strings.Index(equation, "(")
+	for firstBracketIdx >= 0 {
+		additionalBracketCount := 0
+		lastBracketIdx := -1
+		for eidx := firstBracketIdx + 1; eidx < len(equation); eidx++ {
+			cString := string(equation[eidx])
+			if cString == "(" {
+				additionalBracketCount++
+				continue
+			}
+			if cString == ")" && additionalBracketCount > 0 {
+				additionalBracketCount--
+				continue
+			}
+			if cString == ")" && additionalBracketCount == 0 {
+				lastBracketIdx = eidx
+				break
+			}
+		}
+		subProblemResult := solveAdvancedMath(equation[firstBracketIdx+1 : lastBracketIdx])
+		equation = equation[:firstBracketIdx] + fmt.Sprint(subProblemResult) + equation[lastBracketIdx+1:]
+		firstBracketIdx = strings.Index(equation, "(")
+	}
+
+	//solve bracketless equation
+	equationComponents := strings.Split(equation, " ")
+	for len(equationComponents) >= 3 {
+		startIndex := 0
+		for i := 0; i < len(equationComponents); i++ {
+			if equationComponents[i] == "+" {
+				startIndex = i - 1
+				break
+			}
+		}
+		v1, _ := strconv.Atoi(equationComponents[startIndex])
+		opp := equationComponents[startIndex+1]
+		v2, _ := strconv.Atoi(equationComponents[startIndex+2])
+		preComponents := equationComponents[:startIndex]
+		postComponents := equationComponents[startIndex+3:]
+		switch opp {
+		case "+":
+			equationComponents = append(preComponents, fmt.Sprint(v1+v2))
+			break
+		case "*":
+			equationComponents = append(preComponents, fmt.Sprint(v1*v2))
+			break
+		}
+		equationComponents = append(equationComponents, postComponents...)
+	}
+	result, _ := strconv.Atoi(equationComponents[0])
+	return result
+}
+
 func solveP1(inputString string) int {
 	mathProblems := strings.Split(inputString, "\n")
 	count := 0
-
 	for _, mathProblem := range mathProblems {
 		count = count + solveMath(mathProblem)
 	}
+	return count
+}
 
+func solveP2(inputString string) int {
+	mathProblems := strings.Split(inputString, "\n")
+	count := 0
+	for _, mathProblem := range mathProblems {
+		count = count + solveAdvancedMath(mathProblem)
+	}
 	return count
 }
 
@@ -78,8 +140,8 @@ func main() {
 	p1Solution := solveP1(p1Input)
 	fmt.Println(p1Solution)
 
-	// fmt.Println("Solving Part Two!")
-	// p2Input := readInput("input.txt")
-	// p2Solution := solveP2(p2Input)
-	// fmt.Println(p2Solution)
+	fmt.Println("Solving Part Two!")
+	p2Input := readInput("input.txt")
+	p2Solution := solveP2(p2Input)
+	fmt.Println(p2Solution)
 }
--- day18/solve_test.go
+++ day18/solve_test.go
@@ -27,3 +27,26 @@ func TestSolveP1(t *testing.T) {
 		})
 	}
 }
+
+func TestSolveP2(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{`1 + 2 * 3 + 4 * 5 + 6`, 231},
+		{`1 + (2 * 3) + (4 * (5 + 6))`, 51},
+		{`2 * 3 + (4 * 5)`, 46},
+		{`5 + (8 * 3 + 9 + 3 * 4 * 3)`, 1445},
+		{`5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))`, 669060},
+		{`((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2`, 23340},
+	}
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", test.input)
+		t.Run(testname, func(t *testing.T) {
+			ans := solveP2(test.input)
+			if ans != test.want {
+				t.Errorf("got %d, want %d", ans, test.want)
+			}
+		})
+	}
+}
