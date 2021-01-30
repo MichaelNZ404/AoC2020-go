289449dadde3ed3079433f2f9974f3c1bb31d5ff
Michael Coleman <michael.coleman404@gmail.com>
2021-01-30 16:53:02 +1000

    day16 part2

--- day16/solve.go
+++ day16/solve.go
@@ -21,6 +21,13 @@ type ruleRange struct {
 	max int
 }
 
+type ticketField struct {
+	name      string
+	rangeOne  ruleRange
+	rangeTwo  ruleRange
+	positions []int
+}
+
 func solveP1(inputString string) int {
 	sections := strings.Split(inputString, "\n\n")
 	rules := strings.Split(sections[0], "\n")
@@ -58,14 +65,124 @@ func solveP1(inputString string) int {
 	return errorSum
 }
 
+func solveP2(inputString string) int {
+	sections := strings.Split(inputString, "\n\n")
+	rules := strings.Split(sections[0], "\n")
+	ticketFields := make([]ticketField, 0)
+
+	for _, rule := range rules {
+		r, _ := regexp.Compile(`^([\w\s]+): (\d+)-(\d+) or (\d+)-(\d+)$`)
+		digits := r.FindStringSubmatch(rule)
+		fieldName := digits[1]
+		minOne, _ := strconv.Atoi(digits[2])
+		maxOne, _ := strconv.Atoi(digits[3])
+		minTwo, _ := strconv.Atoi(digits[4])
+		maxTwo, _ := strconv.Atoi(digits[5])
+		ticketFields = append(ticketFields, ticketField{name: fieldName, rangeOne: ruleRange{min: minOne, max: maxOne}, rangeTwo: ruleRange{min: minTwo, max: maxTwo}})
+	}
+
+	myTicketString := strings.Split(sections[1], "\n")
+	fields := strings.Split(myTicketString[1], ",")
+	myTicket := make([]int, 0)
+	for _, field := range fields {
+		intField, _ := strconv.Atoi(field)
+		myTicket = append(myTicket, intField)
+	}
+
+	nearbyTickets := strings.Split(sections[2], "\n")
+	validTickets := make([][]int, 0)
+	for i := 1; i < len(nearbyTickets); i++ {
+		ticket := nearbyTickets[i]
+		fields := strings.Split(ticket, ",")
+		intFields := make([]int, 0)
+		for _, field := range fields {
+			intField, _ := strconv.Atoi(field)
+			intFields = append(intFields, intField)
+		}
+
+		valid := true
+		for _, field := range intFields {
+			validField := false
+			for _, ticketField := range ticketFields {
+				if field <= ticketField.rangeOne.max && field >= ticketField.rangeOne.min {
+					validField = true
+					break
+				}
+				if field <= ticketField.rangeTwo.max && field >= ticketField.rangeTwo.min {
+					validField = true
+					break
+				}
+			}
+			if validField == false {
+				valid = false
+				break
+			}
+		}
+		if valid == true {
+			validTickets = append(validTickets, intFields)
+		}
+	}
+
+	for tidx, ticketField := range ticketFields {
+		for idx := 0; idx < len(ticketFields); idx++ { //for every ticket rule, we need to check every ticket field of the valid tickets
+			valid := true
+			for _, validTicket := range validTickets {
+				if validTicket[idx] <= ticketField.rangeOne.max && validTicket[idx] >= ticketField.rangeOne.min {
+					continue
+				}
+				if validTicket[idx] <= ticketField.rangeTwo.max && validTicket[idx] >= ticketField.rangeTwo.min {
+					continue
+				}
+				valid = false
+			}
+			if valid == true {
+				ticketFields[tidx].positions = append(ticketFields[tidx].positions, idx)
+			}
+		}
+	}
+
+	//fields can be valid for multiple, so we need to eliminate other options to determine their correct location
+	tidx := 0
+	claimedPositions := make(map[int]bool)
+	for tidx < len(ticketFields) {
+		ticketField := ticketFields[tidx]
+		if len(ticketField.positions) == 1 && claimedPositions[ticketField.positions[0]] == false {
+			takenPosition := ticketField.positions[0]
+			for oidx, otherTicketField := range ticketFields {
+				if oidx != tidx {
+					newPositions := make([]int, 0)
+					for _, oldPos := range otherTicketField.positions {
+						if oldPos != takenPosition {
+							newPositions = append(newPositions, oldPos)
+						}
+					}
+					ticketFields[oidx].positions = newPositions
+				}
+			}
+			claimedPositions[takenPosition] = true
+			tidx = 0
+			continue
+		}
+		tidx++
+	}
+
+	result := 1
+	for _, ticketField := range ticketFields {
+		if strings.HasPrefix(ticketField.name, "departure") {
+			result = result * myTicket[ticketField.positions[0]]
+		}
+	}
+	return result
+}
+
 func main() {
 	fmt.Println("Solving Part One!")
 	p1Input := readInput("input.txt")
 	p1Solution := solveP1(p1Input)
 	fmt.Println(p1Solution)
 
-	// fmt.Println("Solving Part Two!")
-	// p2Input := readInput("input.txt")
-	// p2Solution := solveP2(p2Input)
-	// fmt.Println(p2Solution)
+	fmt.Println("Solving Part Two!")
+	p2Input := readInput("input.txt")
+	p2Solution := solveP2(p2Input)
+	fmt.Println(p2Solution)
 }
--- day16/solve_test.go
+++ day16/solve_test.go
@@ -33,3 +33,32 @@ nearby tickets:
 		})
 	}
 }
+
+// P2 does not provide a complete test, so adapted it slightly
+func TestSolveP2(t *testing.T) {
+	var tests = []struct {
+		input string
+		want  int
+	}{
+		{`departure: 0-1 or 4-19
+row: 0-5 or 8-19
+seat: 0-13 or 16-19
+
+your ticket:
+11,12,13
+
+nearby tickets:
+3,9,18
+15,1,5
+5,14,9`, 12},
+	}
+	for _, test := range tests {
+		testname := fmt.Sprintf("%s", test.input)
+		t.Run(testname, func(t *testing.T) {
+			ans := solveP2(test.input)
+			if ans != test.want {
+				t.Errorf("got %d, want %d", ans, test.want)
+			}
+		})
+	}
+}
